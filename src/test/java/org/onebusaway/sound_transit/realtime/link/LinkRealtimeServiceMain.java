e03b0efcb13c98a6a0ff482cff15e28a319c585d
Brian Ferris <bdferris@google.com>
2010-11-03 04:07:40 +0000

    That shouldn't be a unit test

--- src/test/java/org/onebusaway/sound_transit/realtime/link/LinkRealtimeServiceMain.java
+++ src/test/java/org/onebusaway/sound_transit/realtime/link/LinkRealtimeServiceMain.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:data-sources.xml"})
-public class LinkRealtimeServiceTest {
+public class LinkRealtimeServiceMain {
 
   private LinkRealtimeService _linkRealtimeService;
 
